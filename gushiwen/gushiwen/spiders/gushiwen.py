b1538599117c0f28909ab407de92bcfb2c74f92c
Bing Han <bing.han@sap.com>
2023-10-21 17:05:13 +0800

    parse all groups of gushiwen and write org files.

--- gushiwen/gushiwen/spiders/gushiwen.py
+++ gushiwen/gushiwen/spiders/gushiwen.py
@@ -1,3 +1,5 @@
+import os
+import io
 import re
 import scrapy
 
@@ -8,25 +10,42 @@ def strip_text(text_list):
             result.append(t.strip())
     return '\n'.join(result)
 
+def write_article_org_file(group_name, title, author, dynasty, text):
+    content = f'''* {title} #card #诗词 #古文
+** {author} {dynasty}
+** {text}
+    '''
+    folder = f'articles/{group_name}'
+    if not os.path.exists(folder): os.makedirs(folder) 
+    with io.open(f'{folder}/{title}.org','w',encoding='utf8') as f:
+        f.write(content)
+
 class GushiwenSpider(scrapy.Spider):
     name = 'gushiwen'
     start_urls = ['https://so.gushiwen.cn/gushi/chuzhong.aspx']
 
     def parse(self, response):
-        seven_first = response.css('div.main3 div.left div.sons div.typecont')[0]
-        yield from response.follow_all(seven_first.css('span a'), callback=self.parse_article)
+        groups = response.css('div.main3 div.left div.sons div.typecont')
+        for group in groups:
+            # group = response.css('div.main3 div.left div.sons div.typecont')[0]
+            group_name = group.css('div.bookMl strong::text').get()
+            yield from response.follow_all(group.css('span a'), callback=self.parse_article, cb_kwargs={'group_name':group_name})
 
-    def parse_article(self, response):
+    def parse_article(self, response, group_name):
         content = response.css('div.main3 .left div#sonsyuanwen.sons div.cont')
         title = content.css('h1::text').get().strip()
         author = content.css('div p.source')
-        author_name = strip_text( author.css('a:nth-child(1)::text').getall() )
-        author_dynasty = strip_text( content.css('a:nth-child(2)::text').getall() )
+        author = strip_text( author.css('a:nth-child(1)::text').getall() )
+        dynasty = re.sub(r'〔|〕', '', strip_text( content.css('a:nth-child(2)::text').getall()))
         text = strip_text( content.css('div.contson::text').getall() + content.css('div.contson p::text').getall() )
+
+        self.log(f'Writing org file of {title}')
+        write_article_org_file(group_name, title, author, dynasty, text)
+
         article = {
             'title' : title,
-            'author_name' : author_name,
-            'author_dynasty': re.sub(r'〔|〕', '', author_dynasty),
+            'author' : author,
+            'dynasty': dynasty,
             'text' : text
         }
         yield article
